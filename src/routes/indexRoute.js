1039d3f5cbe3172242a5d9ec2aef3bda3a9c0898
BDWNAV <rs736611@gmail.com>
2021-07-01 22:58:44 +1000

    Removed the config file for a bit to test

--- src/routes/indexRoute.js
+++ src/routes/indexRoute.js
@@ -1,2 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const config = require("../config-example.json");
+const hypixelAPI = require("hypixel-api");
+const client = new hypixelAPI(config.hypixelApiKey);
+
+router.get('/', (req, res) => {
+    res.render('index');
+});
+
+router.post('/', (req, res) => {
+    client.getPlayer('name', req.body.username).then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        console.log(err);
+    });
+});
+
+module.exports = router;
