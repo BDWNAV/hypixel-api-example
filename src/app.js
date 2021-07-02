9334e5c4d11cbf7eeb4e0af67045e080fc1edd1b
BDWNAV <rs736611@gmail.com>
2021-07-02 11:36:32 +1000

    Switched to different api wrapper

--- src/app.js
+++ src/app.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const config = require("./config-example.json");
-const hypixelAPI = require("hypixel-api");
-const client = new hypixelAPI(config.hypixelApiKey);
 
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'ejs');
@@ -12,8 +9,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const indexRoute = require("./routes/indexRoute");
+const bedwarsRoute = require("./routes/bedwarsRoute");
 
 app.use('/', indexRoute);
+app.use('/bedwars', bedwarsRoute);
 
 app.listen(3000, () => {
     console.log("On http://localhost:3000");
--- src/routes/bedwarsRoute.js
+++ src/routes/bedwarsRoute.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const config = require("../config-example.json");
-const hypixelAPI = require("hypixel-api");
-const client = new hypixelAPI(config.hypixelApiKey);
+const config = require("../config.json");
+const Hypixel = require("hypixel-api-reborn");
+const client = new Hypixel.Client(config.hypixelApiKey);
 
 router.get('/', (req, res) => {
     res.render('bedwars');
-})
+});
+
+router.post('/', (req, res) => {
+    client.getPlayer(req.body.username).then((player) => {
+        console.log(player.stats.bedwars.wins);
+    }).catch((err) => {
+        console.log(err);
+    });
+});
 
 module.exports = router;
