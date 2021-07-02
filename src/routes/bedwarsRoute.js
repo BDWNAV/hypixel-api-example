b66b6edf5ff812cff5119df95c080dd2ec293ae7
BDWNAV <rs736611@gmail.com>
2021-07-02 11:10:14 +1000

    Working on the bedwars route.

--- src/routes/bedwarsRoute.js
+++ src/routes/bedwarsRoute.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const config = require("../config-example.json");
+const hypixelAPI = require("hypixel-api");
+const client = new hypixelAPI(config.hypixelApiKey);
 
+router.get('/', (req, res) => {
+    res.render('bedwars');
+})
 
 module.exports = router;
